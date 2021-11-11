42d528a0d19d2e31c05a4193ec127ae85647c7ef
Nikunj Saraf <nikunjsaraf2001@gmail.com>
2021-11-11 17:07:35 +0530

    First ComputeShader

---
+++ UnityComputeShaders - start/Assets/Scripts/AssignTexture.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public class AssignTexture : MonoBehaviour
+{
+    public ComputeShader shader;
+    public int textResolution = 256;
+
+    private Renderer _renderer;
+    RenderTexture _outputTexture;
+    private int _kernelHandle;
+    
+
+    private void Start()
+    {
+        _outputTexture = new RenderTexture(textResolution, textResolution, 0)
+        {
+            enableRandomWrite = true
+        };
+        _outputTexture.Create();
+
+        _renderer = GetComponent<Renderer>();
+        _renderer.enabled = true;
+        
+        InitShader();
+    }
+
+    private void InitShader()
+    {
+        _kernelHandle = shader.FindKernel("CSMain");
+        shader.SetTexture(_kernelHandle, "Result", _outputTexture);
+        _renderer.material.SetTexture("_MainTex", _outputTexture);
+
+        DispatchShader(textResolution / 16, textResolution / 16);
+    }
+
+    private void DispatchShader(int x, int y)
+    {
+        shader.Dispatch(_kernelHandle, x, y, 1);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.U))
+        {
+            DispatchShader(textResolution / 8, textResolution / 8);
+        }
+    }
+}
